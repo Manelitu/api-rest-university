b2349617b08c81a5d8121cbe0d87e71b9199b68a
Manelito <emanuelwork1@gmail.com>
2023-08-28 17:18:27 -0300

    feat: create spring project

---
+++ src/test/java/com/api/restuniversity/RestUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.restuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
