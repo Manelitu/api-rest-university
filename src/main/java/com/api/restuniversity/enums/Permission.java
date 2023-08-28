1789389d4cff50bea890a0686eb978d8ce1e021d
Manelito <emanuelwork1@gmail.com>
2023-08-28 19:18:32 -0300

    fix: adapt permissions to get on dto

--- src/main/java/com/api/restuniversity/enums/Permission.java
+++ src/main/java/com/api/restuniversity/enums/Permission.java
@@ -1,8 +1,19 @@
 package com.api.restuniversity.enums;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
 public enum Permission {
-    ADMIN,
-    COORDINATOR,
-    PROFESSOR,
-    STUDENT
+    ADMIN("ADMIN"),
+    COORDINATOR("COORDINATOR"),
+    PROFESSOR("PROFESSOR"),
+    STUDENT("STUDENT");
+
+    private final String value;
+
+    private Permission(String value) {
+        this.value = value;
+    }
+
 }
