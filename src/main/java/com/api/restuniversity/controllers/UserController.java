0758acb3e864862eb53acd2c1cd2abfa86849f65
Manelito <emanuelwork1@gmail.com>
2023-08-28 19:21:17 -0300

    refactor: replace create user use-case to service layer

--- src/main/java/com/api/restuniversity/controllers/UserController.java
+++ src/main/java/com/api/restuniversity/controllers/UserController.java
@@ -1,16 +1,11 @@
 package com.api.restuniversity.controllers;
 
 import com.api.restuniversity.dtos.UserDto;
-import com.api.restuniversity.models.UserModel;
 import com.api.restuniversity.services.UserService;
 import jakarta.validation.Valid;
-import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -25,14 +20,8 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<Object> createUser(@RequestBody @Valid UserDto userDto) {
-        var userModel = new UserModel();
-        BeanUtils.copyProperties(userDto, userModel);
-
-        ZoneId zoneid = ZoneId.of("UTC");
-        LocalDateTime now = LocalDateTime.now(zoneid);
-
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(userService.create(userModel));
+                .body(userService.create(userDto));
     }
 }
