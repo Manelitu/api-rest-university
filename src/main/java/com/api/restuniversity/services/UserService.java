cdde5994b857f010a918cc261b86229f8355a0f5
Manelito <emanuelwork1@gmail.com>
2023-08-28 19:38:35 -0300

    feat: create list all use case

--- src/main/java/com/api/restuniversity/services/UserService.java
+++ src/main/java/com/api/restuniversity/services/UserService.java
@@ -1,9 +1,20 @@
 package com.api.restuniversity.services;
 
+import com.api.restuniversity.dtos.UserDto;
+import com.api.restuniversity.enums.Permission;
+import com.api.restuniversity.exceptions.BadRequestException;
+import com.api.restuniversity.exceptions.ConflictException;
 import com.api.restuniversity.models.UserModel;
 import com.api.restuniversity.repositories.UserRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -14,8 +25,22 @@ public class UserService {
     }
 
     @Transactional
-    public UserModel create(UserModel userModel) {
+    public UserModel create(UserDto userDto) throws ConflictException, BadRequestException {
+        var userModel = new UserModel();
+        Permission permission = Permission.valueOf(userDto.getPermissions());
+
+        BeanUtils.copyProperties(userDto, userModel);
+
+        userModel.setPermissions(permission);
+
+        ZoneId zoneid = ZoneId.of("UTC");
+        LocalDateTime now = LocalDateTime.now(zoneid);
+
         return userRepository.save(userModel);
+    }
+
+    public List<UserModel> list() {
+        return userRepository.findAll();
     }
 }
 
