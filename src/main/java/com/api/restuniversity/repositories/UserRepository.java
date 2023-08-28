5a6e71d41deb28544359310a1d112f744f96b5bc
Manelito <emanuelwork1@gmail.com>
2023-08-28 18:17:40 -0300

    fix: replace user repository on parameters type to user model

--- src/main/java/com/api/restuniversity/repositories/UserRepository.java
+++ src/main/java/com/api/restuniversity/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.api.restuniversity.repositories;
 
+import com.api.restuniversity.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserRepository, UUID> {}
+public interface UserRepository extends JpaRepository<UserModel, UUID> {}
