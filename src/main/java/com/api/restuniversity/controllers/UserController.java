fe0ea9a2d7826a0abc00fd23019cb6b31a966209
Manelito <emanuelwork1@gmail.com>
2023-08-28 18:28:11 -0300

    feat: create route of createUser

--- src/main/java/com/api/restuniversity/controllers/UserController.java
+++ src/main/java/com/api/restuniversity/controllers/UserController.java
@@ -1,9 +1,16 @@
 package com.api.restuniversity.controllers;
 
+import com.api.restuniversity.dtos.UserDto;
+import com.api.restuniversity.models.UserModel;
 import com.api.restuniversity.services.UserService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -14,5 +21,18 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createUser(@RequestBody @Valid UserDto userDto) {
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userDto, userModel);
+
+        ZoneId zoneid = ZoneId.of("UTC");
+        LocalDateTime now = LocalDateTime.now(zoneid);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(userService.create(userModel));
     }
 }
