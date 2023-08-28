8306c7ede6cbe597456bb81ca0c245e1ded9140c
Manelito <emanuelwork1@gmail.com>
2023-08-28 19:44:44 -0300

    feat: create list users route

--- src/main/java/com/api/restuniversity/controllers/UserController.java
+++ src/main/java/com/api/restuniversity/controllers/UserController.java
@@ -3,8 +3,13 @@ package com.api.restuniversity.controllers;
 import com.api.restuniversity.dtos.UserDto;
 import com.api.restuniversity.exceptions.BadRequestException;
 import com.api.restuniversity.exceptions.ConflictException;
+import com.api.restuniversity.models.UserModel;
 import com.api.restuniversity.services.UserService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,9 +26,23 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> createUser(@RequestBody @Valid UserDto userDto) throws BadRequestException, ConflictException {
+    public ResponseEntity<UserModel> createUser(@RequestBody @Valid UserDto userDto) throws BadRequestException, ConflictException {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(userService.create(userDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserModel>> listUsers(
+            @PageableDefault(
+                    page = 0,
+                    size = 10,
+                    sort = "id",
+                    direction = Sort.Direction.ASC
+            ) Pageable pageable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userService.list(pageable));
+
     }
 }
