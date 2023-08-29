05d5b673bb4923b94b0c073b5071db76f423491e
Manelito <emanuelwork1@gmail.com>
2023-08-28 21:48:46 -0300

    fix: adjust on validation update user dto

--- src/main/java/com/api/restuniversity/dtos/users/UpdateUserDto.java
+++ src/main/java/com/api/restuniversity/dtos/users/UpdateUserDto.java
@@ -2,12 +2,15 @@ package com.api.restuniversity.dtos.users;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.util.Optional;
 
 @Data
 public class UpdateUserDto {
-    @NotBlank
     @Email
+    @NotBlank
     private String email;
     @NotBlank
     private String name;
