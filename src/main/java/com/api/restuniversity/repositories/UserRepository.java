21e73649910ff3f4fc018198e4540d33f0d0676b
Manelito <emanuelwork1@gmail.com>
2023-08-29 18:43:46 -0300

    feat: create findByEmail method to authorization

--- src/main/java/com/api/restuniversity/repositories/UserRepository.java
+++ src/main/java/com/api/restuniversity/repositories/UserRepository.java
@@ -2,9 +2,12 @@ package com.api.restuniversity.repositories;
 
 import com.api.restuniversity.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, UUID> {}
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    UserDetails findByEmail(String email);
+}
