7784ae831522ebec9ad28f1d6de21089b418c927
Manelito <emanuelwork1@gmail.com>
2023-08-29 18:44:08 -0300

    feat: create Authorization service

---
+++ src/main/java/com/api/restuniversity/services/AuthorizationService.java
@@ -0,0 +1,22 @@
+package com.api.restuniversity.services;
+
+import com.api.restuniversity.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    final UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
+    }
+}
