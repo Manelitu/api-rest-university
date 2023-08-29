b2cf6d20e51606a6b37d4d1fe0f299b12dfc3f02
Manelito <emanuelwork1@gmail.com>
2023-08-29 20:24:53 -0300

    feat: return token after login

--- src/main/java/com/api/restuniversity/controllers/AuthController.java
+++ src/main/java/com/api/restuniversity/controllers/AuthController.java
@@ -1,6 +1,9 @@
 package com.api.restuniversity.controllers;
 
 import com.api.restuniversity.dtos.auth.AuthDto;
+import com.api.restuniversity.dtos.auth.LoginResponseDto;
+import com.api.restuniversity.models.UserModel;
+import com.api.restuniversity.services.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,16 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     final AuthenticationManager authenticationManager;
+    final TokenService tokenService;
 
-    public AuthController(AuthenticationManager authenticationManager) {
+    public AuthController(AuthenticationManager authenticationManager, TokenService tokenService) {
         this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody @Valid AuthDto authDto) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(authDto.getLogin(), authDto.getPassword());
         var auth = this.authenticationManager.authenticate(usernamePassword);
+        String token = tokenService.generateToken((UserModel) auth.getPrincipal());
+        var response = new LoginResponseDto(token);
 
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
