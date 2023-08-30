ac66055224b7596ef2d3c809f5f982a0bae6b5fd
Manelito <emanuelwork1@gmail.com>
2023-08-30 20:47:19 -0300

    feat: add correct disciplinesId to join table

--- src/main/java/com/api/restuniversity/dtos/periods/PeriodDto.java
+++ src/main/java/com/api/restuniversity/dtos/periods/PeriodDto.java
@@ -2,10 +2,10 @@ package com.api.restuniversity.dtos.periods;
 
 import lombok.Data;
 
-import java.util.List;
 import java.util.UUID;
 
 @Data
 public class PeriodDto {
-    private List<UUID> disciplines;
+    private Integer period;
+    private UUID disciplineId;
 }
