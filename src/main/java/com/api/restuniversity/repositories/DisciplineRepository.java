ae3fc1ea53b442f8349573a8512007c0397db636
Manelito <emanuelwork1@gmail.com>
2023-08-30 20:46:28 -0300

    feat: create methods to search

--- src/main/java/com/api/restuniversity/repositories/DisciplineRepository.java
+++ src/main/java/com/api/restuniversity/repositories/DisciplineRepository.java
@@ -4,9 +4,13 @@ import com.api.restuniversity.models.DisciplineModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface DisciplineRepository extends JpaRepository<DisciplineModel, UUID> {
     Boolean existsByName(String name);
+
+    Optional<DisciplineModel> findByName(String name);
+
 }
--- src/main/java/com/api/restuniversity/repositories/PeriodRepository.java
+++ src/main/java/com/api/restuniversity/repositories/PeriodRepository.java
@@ -4,6 +4,7 @@ import com.api.restuniversity.models.PeriodModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
