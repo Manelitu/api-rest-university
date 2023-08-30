0cafcf977eb5c7de484b40be05b6e7cdb55e516f
Manelito <emanuelwork1@gmail.com>
2023-08-30 14:48:58 -0300

    feat: create subject dto

---
+++ src/main/java/com/api/restuniversity/dtos/subjects/CreateSubjectDto.java
@@ -0,0 +1,15 @@
+package com.api.restuniversity.dtos.subjects;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class CreateSubjectDto {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private UUID courseId;
+}
