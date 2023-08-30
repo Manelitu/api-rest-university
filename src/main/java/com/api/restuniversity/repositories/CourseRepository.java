df0a621a516614cd2edb88338f3b5110509d1316
Manelito <emanuelwork1@gmail.com>
2023-08-29 21:30:45 -0300

    feat: adding Repository bean

--- src/main/java/com/api/restuniversity/repositories/CourseRepository.java
+++ src/main/java/com/api/restuniversity/repositories/CourseRepository.java
@@ -2,8 +2,10 @@ package com.api.restuniversity.repositories;
 
 import com.api.restuniversity.models.CourseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface CourseRepository extends JpaRepository<CourseModel, UUID> {
+    CourseModel findByName(String name);
 }
--- src/main/java/com/api/restuniversity/repositories/SubjectRepository.java
+++ src/main/java/com/api/restuniversity/repositories/SubjectRepository.java
@@ -2,8 +2,10 @@ package com.api.restuniversity.repositories;
 
 import com.api.restuniversity.models.SubjectModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface SubjectRepository extends JpaRepository<SubjectModel, UUID> {
 }
