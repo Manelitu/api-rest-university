fc4e6b8606f31d5630775a9dc1b3566cdd7b1d56
Manelito <emanuelwork1@gmail.com>
2023-08-29 21:24:43 -0300

    feat: create course repository using jpa

---
+++ src/main/java/com/api/restuniversity/repositories/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.api.restuniversity.repositories;
+
+import com.api.restuniversity.models.CourseModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CourseRepository extends JpaRepository<CourseModel, UUID> {
+}
