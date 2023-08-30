f957fe0a3b7e5db5ef5053a9d25177863858b26a
Manelito <emanuelwork1@gmail.com>
2023-08-30 20:46:44 -0300

    feat: create Period controller

---
+++ src/main/java/com/api/restuniversity/controllers/PeriodController.java
@@ -0,0 +1,66 @@
+package com.api.restuniversity.controllers;
+
+import com.api.restuniversity.dtos.periods.PeriodDto;
+import com.api.restuniversity.exceptions.BadRequestException;
+import com.api.restuniversity.exceptions.ConflictException;
+import com.api.restuniversity.exceptions.NotFoundException;
+import com.api.restuniversity.models.PeriodModel;
+import com.api.restuniversity.services.PeriodService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/period")
+public class PeriodController {
+
+    final private PeriodService periodService;
+
+    public PeriodController(PeriodService periodService) {
+        this.periodService = periodService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody @Valid PeriodDto params) throws ConflictException, BadRequestException {
+        return ResponseEntity.ok(periodService.create(params));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PeriodModel>> list(
+            @PageableDefault(
+                    page = 0,
+                    size = 10,
+                    sort = "period",
+                    direction = Sort.Direction.ASC
+            ) Pageable pageable) {
+        return ResponseEntity.ok(periodService.list(pageable));
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<PeriodModel>> listById(@PathVariable("id") UUID id) throws NotFoundException, BadRequestException {
+        return ResponseEntity.ok(periodService.listById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PeriodModel> delete(
+            @PathVariable(value = "id") UUID id
+    ) throws NotFoundException {
+        return ResponseEntity.ok(periodService.delete(id));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> update(
+            @PathVariable(value = "id") UUID id,
+            @RequestBody @Valid PeriodDto params
+    ) throws NotFoundException, BadRequestException {
+        return ResponseEntity.ok(periodService.update(id, params));
+    }
+}
