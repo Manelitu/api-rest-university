8e415d1d6bf1c1b7f5de96b6e797cdfc231ed84c
Manelito <emanuelwork1@gmail.com>
2023-08-29 22:14:22 -0300

    feat: create method existsByname on Hibernate

--- src/main/java/com/api/restuniversity/repositories/CourseRepository.java
+++ src/main/java/com/api/restuniversity/repositories/CourseRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 @Repository
-public interface CourseRepository extends JpaRepository<CourseModel, UUID> {}
+public interface CourseRepository extends JpaRepository<CourseModel, UUID> {
+    Boolean existsByName(String name);
+}
