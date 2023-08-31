735a6e1f60416be40bac8d1d9c384b1e44218c4e
Manelito <emanuelwork1@gmail.com>
2023-08-31 11:55:50 -0300

    feat: configure cors

--- src/main/java/com/api/restuniversity/configs/security/SecurityConfig.java
+++ src/main/java/com/api/restuniversity/configs/security/SecurityConfig.java
@@ -40,6 +40,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.DELETE, "/api/v1/courses", "/api/v1/discipline", "/api/v1/period").hasAuthority("ROLE_COORDINATOR")
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors()
+                .and()
                 .build();
     }
 
